fa101f43d1bf40eb5c595be713947f0c08004bef
hwd3527 <hwd3527@naver.com>
2020-02-24 22:24:40 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,54 +1,77 @@
-import React, { Component } from 'react';
-import './App.css';
-import TOC from './components/TOC';
-import Subject from './components/Subject';
-import Content from './components/Content';
+import React, { Component } from "react";
+import "./App.css";
+import TOC from "./components/TOC";
+import Subject from "./components/Subject";
+import ReadContent from "./components/ReadContent";
+import Control from "./components/Control";
+import CreateContent from "./components/CreateContent";
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.max_content_id = 3;
     this.state = {
-      mode:'read',
-      selected_content_id:1,
+      mode: "create",
+      selected_content_id: 1,
       subject: {
-        title:'WEB',
-        sub:'World Wide Web!',
+        title: "WEB",
+        sub: "World Wide Web!"
       },
-      welcome:{
-        title:'Welcome',
-        desc:'Hello, React!!',
+      welcome: {
+        title: "Welcome",
+        desc: "Hello, React!!"
       },
       contents: [
-        { id: 1, title: 'HTML', desc: 'HTML is for information' },
-        { id: 2, title: 'CSS', desc: 'CSS is for design' },
-        { id: 3, title: 'Javascript', desc: 'Javascript is for interactive' },
+        { id: 1, title: "HTML", desc: "HTML is for information" },
+        { id: 2, title: "CSS", desc: "CSS is for design" },
+        { id: 3, title: "Javascript", desc: "Javascript is for interactive" }
       ]
-    }
+    };
   }
   render() {
-    var _title, _desc = null;
-    if(this.state.mode === 'welcome') {
+    var _title,
+      _desc,
+      _article = null;
+    if (this.state.mode === "welcome") {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
-    } else if(this.state.mode === 'read') {
-      var i=0;
-      while(i<this.state.contents.length) {
-        if(this.state.contents[i].id === this.state.selected_content_id){
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
+    } else if (this.state.mode === "read") {
+      var i = 0;
+      while (i < this.state.contents.length) {
+        if (this.state.contents[i].id === this.state.selected_content_id) {
           _title = this.state.contents[i].title;
           _desc = this.state.contents[i].desc;
           break;
         }
-        i=i+1;        
-      }  
+        i = i + 1;
+      }
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
+    } else if (this.state.mode === "create") {
+      _article = (
+        <CreateContent
+          onSubmit={function(_title, _desc) {
+            this.max_content_id = this.max_content_id + 1;
+            var _contents = this.state.contents.concat({
+              id: this.max_content_id,
+              title: _title,
+              desc: _desc
+            });
+            this.setState({
+              contents: _contents
+            });
+          }.bind(this)}
+        ></CreateContent>
+      );
     }
     return (
       <div className="App">
         Hello, React!
-        <Subject 
-          title={this.state.subject.title} 
+        <Subject
+          title={this.state.subject.title}
           sub={this.state.subject.sub}
-          onChangPage={function(){
-            this.setState({mode:'welcome'});
+          onChangPage={function() {
+            this.setState({ mode: "welcome" });
           }.bind(this)}
         ></Subject>
         {/* <header>
@@ -62,14 +85,23 @@ class App extends Component {
           }.bind(this)}>{this.state.subject.title}</a></h1>
           {this.state.subject.sub}
         </header> */}
-        <Subject title="React" sub="For Ui"></Subject>
-        <TOC onChangePage={function(id){
-          this.setState({
-            mode:'read',
-            selected_content_id:Number(id),
-          })
-        }.bind(this)} data={this.state.contents}></TOC>
-        <Content title={_title} desc={_desc}></Content>
+        <TOC
+          onChangePage={function(id) {
+            this.setState({
+              mode: "read",
+              selected_content_id: Number(id)
+            });
+          }.bind(this)}
+          data={this.state.contents}
+        ></TOC>
+        <Control
+          onChangeMode={function(_mode) {
+            this.setState({
+              mode: _mode
+            });
+          }.bind(this)}
+        ></Control>
+        {_article}
       </div>
     );
   }
